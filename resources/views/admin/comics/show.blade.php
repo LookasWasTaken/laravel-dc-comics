ed1031f3692feeb35abee9dbeea542e23762fef1
LookasWasTaken~ <piacen.luca@gmail.com>
2023-05-30 20:31:12 +0200

    ADD delete button on show route

--- resources/views/admin/comics/show.blade.php
+++ resources/views/admin/comics/show.blade.php
@@ -12,6 +12,32 @@
     <div class="d-flex justify-content-center align-items-center my-3 gap-3">
         <a class="btn btn-warning text-uppercase fw-bold" href="{{route('comics.edit', $comic->id)}}">edit</a>
         <a class="btn btn-secondary text-uppercase fw-bold" href="{{route('comics.index')}}">back</a>
+        <button type="button" class="btn btn-danger text-uppercase fw-bold" data-bs-toggle="modal" data-bs-target="#modal-{{$comic->id}}">delete</button>
+                        <div class="modal fade" id="modal-{{$comic->id}}" tabindex="-1" data-bs-backdrop="static" data-bs-keyboard="false" role="dialog" aria-labelledby="modalTitle-{{$comic->id}}" aria-hidden="true">
+                            <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-sm" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header d-flex flex-column">
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="close"></button>
+                                        <h5 class="text-muted fs-6 text-uppercase">You are going to delete</h5>
+                                        <h5 class="modal-title mb-2 text-uppercase fw-bold" id="modalTitle-{{$comic->id}}">{{$comic->title}}</h5>
+                                        <h5 class="modal-title mb-2 fs-6 text-muted" id="modalTitle-{{$comic->id}}">No. {{$comic->id}}</h5>
+                                        <img width="120" src="{{$comic->thumb}}" alt="">
+                                    </div>
+                                    <div class="modal-body">
+                                        <p class="mb-0 text-danger text-uppercase">Once confirmed, there</p>
+                                        <p class="mb-0 text-danger text-uppercase">will be no going back</p>
+                                    </div>
+                                    <div class="modal-footer d-flex justify-content-center align-items-center gap-2">
+                                        <form action="{{route('comics.destroy', $comic->id)}}" method="post">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="btn btn-danger">Confirm</button>
+                                        </form>
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" aria-label="close">Return</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
     </div>
     <div class="card w-50 mx-auto">
         <div class="card-header rounded">
